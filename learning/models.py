61c17c20e17e3db539f5efa662259d68375e8eb1
ali <ali.nakhaee.kooshe@gmail.com>
2024-04-06 15:54:54 +0330

    practice and constantText models created in learning app

--- learning/models.py
+++ learning/models.py
@@ -1,2 +1,19 @@
 """ learning.models file """
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+
+class Practice(models.Model):
+    """ Base model for each learning course """
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+class ConstantText(models.Model):
+    """ Constant texts for each practice """
+    practice = models.ForeignKey(Practice, on_delete=models.CASCADE)
+    text = models.TextField()
+    step = models.PositiveIntegerField()
--- math_learn_web/settings.py
+++ math_learn_web/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    "users",
+    "learning",
+
+    # Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# My settings
+LOGIN_URL = 'users:login'
+AUTH_USER_MODEL = 'users.User'
