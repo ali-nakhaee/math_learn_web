e2c498516befe9823c452cde4795087c4321d8d8
ali <ali.nakhaee.kooshe@gmail.com>
2024-04-08 22:55:47 +0330

    add_practice view post method completed

--- learning/views.py
+++ learning/views.py
@@ -3,12 +3,31 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Practice, ConstantText, Answer, Help
+from users.models import User
 
+sample_user = User.objects.get(username='Ali')
 
 class AddPractice(View):
     def get(self, request):
         return render(request, 'learning/add_practice.html')
     
     def post(self, request):
-        fields = request.POST.dict()
-        return HttpResponse(fields.values())
+        fields = request.POST.keys()
+        practice = Practice.objects.create(teacher=sample_user)
+        for field in fields:
+            field_name = field.split('_')[0]
+            if field_name == 'text':
+                step = field.split('_')[1]
+                text = request.POST.get(field)
+                ConstantText.objects.create(practice=practice, text=text, step=step)
+            elif field_name == 'answer':
+                step = field.split('_')[1]
+                true_answer = request.POST.get(field)
+                Answer.objects.create(practice=practice, true_answer=true_answer, step=step)
+            elif field_name == 'help':
+                step = field.split('_')[1]
+                text = request.POST.get(field)
+                Help.objects.create(practice=practice, text=text, step=step)
+                
+        return HttpResponse(practice)
