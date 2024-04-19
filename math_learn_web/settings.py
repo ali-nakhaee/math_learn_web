85c6310e560e1dff2431d26fa9916fe51ecc4c64
ali <ali.nakhaee.kooshe@gmail.com>
2024-04-19 17:38:08 +0330

    testing reportlab to make pdfs.

--- math_learn_web/settings.py
+++ math_learn_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wl5&d2bj%9mg$#n!i_6k(ol0mxq$gtiks@krrd2u7&u@+nx(%9"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
