b89549db23bd218462785a3413a9d63f36754e7e
ali <ali.nakhaee.kooshe@gmail.com>
2024-04-21 23:24:15 +0330

    testing pdfkit

--- pdf_test.py
+++ pdf_test.py
@@ -8,7 +8,9 @@ from reportlab.pdfbase.ttfonts import TTFont
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # reportlab.rl_config.TTFSearchPath.append(str(settings.BASE_DIR) + '/learning/lib/reportlabs/fonts')
 
-reportlab.rl_config.TTFSearchPath.append(str(BASE_DIR))
+# reportlab.rl_config.TTFSearchPath.append(str(BASE_DIR))
+
+# need to copy font file to /"v_env"/lib/python3.12/site-packages/reportlab/fonts
 pdfmetrics.registerFont(TTFont('samim', 'samim.ttf'))
 
 c = canvas.Canvas("hello_again.pdf", pagesize=(595.27, 841.89))
