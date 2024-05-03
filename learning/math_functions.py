4105662858889d7fee788d305abbd96a37a94f5a
ali <ali.nakhaee.kooshe@gmail.com>
2024-05-03 13:43:56 +0330

    signal created for change sample_questions after change in base_question

--- learning/math_functions.py
+++ learning/math_functions.py
@@ -4,13 +4,13 @@ from .models import Question
 
 
 def make_sample_question(base_question_id):
-        """ Function to make sample question from base question """
-
-        base_question = Question.objects.get(id=base_question_id)
-        sample_variable = random.randint(base_question.variable_min, base_question.variable_max)
-        sample_question_text = base_question.text.replace(base_question.variable, str(sample_variable))
-        expression = base_question.true_answer
-        x = symbols(base_question.variable)
-        answer_function = sympify(expression)
-        sample_question_answer = float(answer_function.subs(x, sample_variable))
-        return {"text": sample_question_text, "answer": sample_question_answer}
+    """ Function to make sample question from base question """
+    
+    base_question = Question.objects.get(id=base_question_id)
+    sample_variable = random.randint(base_question.variable_min, base_question.variable_max)
+    sample_question_text = base_question.text.replace(base_question.variable, str(sample_variable))
+    expression = base_question.true_answer
+    x = symbols(base_question.variable)
+    answer_function = sympify(expression)
+    sample_question_answer = float(answer_function.subs(x, sample_variable))
+    return {"text": sample_question_text, "answer": sample_question_answer}
--- learning/signals.py
+++ learning/signals.py
@@ -2,11 +2,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from .models import Question, SampleQuestion
+from .math_functions import make_sample_question
 
 
 @receiver(post_save, sender=Question)
 def change_sample_questions(sender, instance, created, **kwargs):
     if not created:
         sample_questions = SampleQuestion.objects.filter(base_question=instance)
-        
-    
+        for sample_question in sample_questions:
+            new_sample_question = make_sample_question(instance.id)
+            sample_question.text = new_sample_question["text"]
+            sample_question.true_answer = new_sample_question["answer"]
+            sample_question.save()
+            
